16252fffd40169efdbb17b3148c4df233fda517b
Vivien <vivientopou57@gmail.com>
2024-07-26 13:50:19 +0000

    commit

--- app/Http/Livewire/Admin/AdminAddServiceComponent.php
+++ app/Http/Livewire/Admin/AdminAddServiceComponent.php
@@ -2,12 +2,94 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Service;
+use App\Models\ServiceCategory;
+use Carbon\Carbon;
 use Livewire\Component;
+use Illuminate\Support\Str;
+use Livewire\WithFileUploads;
 
 class AdminAddServiceComponent extends Component
 {
+    use WithFileUploads;
+    
+    public $name;
+    public $slug;
+    public $tagline;
+    public $service_category_id;
+    public $price;
+    public $discount;
+    public $discount_type;
+    public $image;
+    public $thumbnail;
+    public $description;
+    public $inclusion;
+    public $exclusion;
+    public $status;
+    
+
+    public function generateSlug()
+    {
+        $this->slug = Str::slug($this->name, '-');
+    }
+
+    public function updated($fields)
+    {
+        $this->validateOnly($fields, [
+            'name' => 'required',
+            'slug' => 'required',
+            'tagline' => 'required',
+            'service_category_id' => 'required',            
+            'image' => 'required|mimes:png,jpg',
+            'thumbnail' => 'required|mimes:png,jpg',
+            'description' => 'required',
+            'inclusion' => 'required',
+            'exclusion' => 'required',            
+        ]);
+    }
+
+    public function createService ()
+    {
+        $this->validate([
+            'name' => 'required',
+            'slug' => 'required',
+            'tagline' => 'required',
+            'service_category_id' => 'required',
+            'price' => 'required',            
+            'image' => 'required|mimes:png,jpg',
+            'thumbnail' => 'required|mimes:png,jpg',
+            'description' => 'required',
+            'inclusion' => 'required',
+            'exclusion' => 'required',            
+        ]);
+
+        $service = new Service();
+        $service->name = $this->name;
+        $service->slug = $this->slug;
+        $service->tagline = $this->tagline;
+        $service->service_category_id = $this->service_category_id;
+        $service->price = $this->price;
+        $service->description = $this->description;
+
+        $service->inclusion = str_replace("\n",'|',trim($this->inclusion));
+        $service->exclusion = str_replace("\n",'|',trim($this->exclusion));
+        
+        $imageName = Carbon::now()->timestamp . '.' . $this->thumbnail->extension();
+        $this->thumbnail->storeAs('services/thumbnails', $imageName);
+        $service->thumbnail = $imageName;
+
+        $imageName2 = Carbon::now()->timestamp . '.' . $this->image->extension();
+        $this->image->storeAs('services', $imageName2);
+        $service->image = $imageName2;
+        
+        $service->save();
+        session()->flash('message', 'Service créé avec succes !');
+    }
+
     public function render()
     {
-        return view('livewire.admin.admin-add-service-component')->layout('layouts.base');
+        $categories = ServiceCategory::all();
+
+        return view('livewire.admin.admin-add-service-component', ['categories'=>$categories])->layout('layouts.base');
     }
 }
